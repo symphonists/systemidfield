56a01d2dd2471462376f902022059808dbd7cd7c
Nicolas Brassard <nitriques@users.noreply.github.com>
2017-08-22 17:35:16 -0400

    Symphony 2.x LTS support only

--- fields/field.systemid.php
+++ fields/field.systemid.php
@@ -18,17 +18,7 @@
 		}
 
 		public function createTable() {
-			$field_id = $this->get('id');
-
-			return Symphony::Database()->query("
-				CREATE TABLE IF NOT EXISTS `tbl_entries_data_{$field_id}` (
-					`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-					`entry_id` INT(11) UNSIGNED NOT NULL,
-					`value` INT(11) UNSIGNED DEFAULT NULL,
-					PRIMARY KEY (`id`),
-					KEY `entry_id` (`entry_id`)
-				) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-			");
+			return true;
 		}
 
 		public function canFilter() {
@@ -43,98 +33,88 @@
 			return false;
 		}
 
+		public function commit() {
+			// if the default implementation works...
+			if(!parent::commit()) {
+				return false;
+			}
+
+			$id = $this->get('id');
+
+			// exit if there is no id
+			if ($id == false) {
+				return false;
+			}
+
+			$settings = array();
+			$settings['field_id'] = $id;
+			return FieldManager::saveSettings($id, $settings);
+		}
+
 	/*-------------------------------------------------------------------------
 		Settings:
 	-------------------------------------------------------------------------*/
 
-		public function displaySettingsPanel(&$wrapper, $errors = null, $append_before = null, $append_after = null) {
+		public function displaySettingsPanel(XMLElement &$wrapper, $errors = null)
+		{
 			parent::displaySettingsPanel($wrapper, $errors);
 			$this->appendShowColumnCheckbox($wrapper);
-		}
-
-		public function buildSummaryBlock($errors = null) {
-			$order = $this->get('sortorder');
-			$div = new XMLElement('div');
-			$div->appendChild(Widget::Input(
-				"fields[{$order}][location]",
-				'main', 'hidden'
-			));
-
-			$label = Widget::Label(__('Label'));
-			$label->appendChild(Widget::Input(
-				"fields[{$order}][label]",
-				$this->get('label')
-			));
-
-			if (isset($errors['label'])) {
-				$label = Widget::Error($label, $errors['label']);
-			}
-
-			$div->appendChild($label);
-
-			return $div;
 		}
 
 	/*-------------------------------------------------------------------------
 		Publish:
 	-------------------------------------------------------------------------*/
 
-		public function displayPublishPanel(&$wrapper, $data = null, $error = null, $prefix = null, $postfix = null) {
+		public function displayPublishPanel(XMLElement &$wrapper, $data = null, $flagWithError = null, $fieldnamePrefix = null, $fieldnamePostfix = null, $entry_id = null)
+		{
 			$label = Widget::Label($this->get('label'));
-			$text = new XMLElement('div', $data['value']);
+			$text = new XMLElement('div', $entry_id);
 
 			$wrapper->appendChild($label);
-			$wrapper->appendChild($text);
+			$label->appendChild($text);
 		}
 
 	/*-------------------------------------------------------------------------
 		Input:
 	-------------------------------------------------------------------------*/
 
-		public function checkPostFieldData($data, &$message, $entry_id = null) {
+		public function checkPostFieldData($data, &$message, $entry_id = null)
+		{
 			$message = null;
 
 			return self::__OK__;
 		}
 
-		public function processRawFieldData($data, &$status, $simulate = false, $entry_id = null) {
+		public function processRawFieldData($data, &$status, $simulate = false, $entry_id = null)
+		{
 			$status = self::__OK__;
 
-			return array(
-				'value'		=> $entry_id
-			);
+			return $data;
 		}
 
 	/*-------------------------------------------------------------------------
 		Output:
 	-------------------------------------------------------------------------*/
 
-		public function appendFormattedElement(&$wrapper, $data, $encode = false, $mode = null) {
+		public function appendFormattedElement(&$wrapper, $data, $encode = false, $mode = null)
+		{
 			$element = new XMLElement($this->get('element_name'));
 			$element->setAttribute('hash', @dechex($data['value']));
 			$element->setValue(@$data['value'] ? $data['value'] : '0');
 			$wrapper->appendChild($element);
 		}
 
-		public function prepareReadableValue($data, $entry_id = null, $truncate = false, $defaultValue = null) {
-			return isset($data['value']) ? $data['value'] : $entry_id;
-		}
-
-		public function prepareTableValue($data, XMLElement $link = null, $entry_id = null) {
-			if (is_null($entry_id)) return;
-
-			return parent::prepareTableValue(
-				array(
-					'value'		=> $entry_id
-				), $link
-			);
+		public function prepareTextValue($data, $entry_id = null)
+		{
+			return $entry_id;
 		}
 
 	/*-------------------------------------------------------------------------
 		Filtering:
 	-------------------------------------------------------------------------*/
 
-		public function displayDatasourceFilterPanel(&$wrapper, $data = null, $errors = null, $prefix = null, $postfix = null) {
+		public function displayDatasourceFilterPanel(&$wrapper, $data = null, $errors = null, $prefix = null, $postfix = null)
+		{
 			$wrapper = new XMLElement('p');
 			$wrapper->setAttribute('style', 'display: none;');
 		}
@@ -143,11 +123,13 @@
 		Sorting:
 	-------------------------------------------------------------------------*/
 
-		public function buildSortingSQL(&$joins, &$where, &$sort, $order = 'ASC') {
+		public function buildSortingSQL(&$joins, &$where, &$sort, $order = 'ASC')
+		{
 			$sort = 'ORDER BY ' . (strtolower($order) == 'random' ? 'RAND()' : "`e`.`id` {$order}");
 		}
 
-		public function buildSortingSelectSQL($sort, $order = 'ASC') {
+		public function buildSortingSelectSQL($sort, $order = 'ASC')
+		{
 			return null;
 		}
 	}
